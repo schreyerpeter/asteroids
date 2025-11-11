e681818eab5afd67fa35a005528e043e73fc5724
schreyerpeter <prhschreyer@gmail.com>
2025-11-11 10:40:46 -0800

    feat: draw player

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0
 
@@ -16,6 +18,7 @@ def main():
 
         log_state()
         screen.fill('black')
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000  # Limit to 60 FPS
 
